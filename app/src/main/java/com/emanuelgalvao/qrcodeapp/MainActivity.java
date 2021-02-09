a02f392251601090fc12a0ebc3476b3295ea0b3e
Emanuel Rodrigues Galvão <31593066+emanuelgalvao@users.noreply.github.com>
2021-02-08 21:56:42 -0300

    Created menu actions

--- app/src/main/java/com/emanuelgalvao/qrcodeapp/MainActivity.java
+++ app/src/main/java/com/emanuelgalvao/qrcodeapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.emanuelgalvao.qrcodeapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -10,9 +12,15 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.Objects;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +29,21 @@ public class MainActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(onItemSelect());
+
+        ImageView ivBack = navigationView.getHeaderView(0).findViewById(R.id.ivBack);
         ImageView ivMenu = findViewById(R.id.ivMenu);
         LinearLayout llRead = findViewById(R.id.llRead);
         LinearLayout llGenerate = findViewById(R.id.llGenerate);
 
+        ivBack.setOnClickListener(onClickButton());
         ivMenu.setOnClickListener(onClickButton());
         llRead.setOnClickListener(onClickButton());
         llGenerate.setOnClickListener(onClickButton());
@@ -36,7 +55,10 @@ public class MainActivity extends AppCompatActivity {
         return v -> {
             switch (v.getId()) {
                 case R.id.ivMenu:
-                    Toast.makeText(this, "Ola", Toast.LENGTH_SHORT).show();
+                    drawerLayout.openDrawer(navigationView);
+                    break;
+                case R.id.ivBack:
+                    drawerLayout.closeDrawer(navigationView);
                     break;
                 case R.id.llRead:
                     openActivity(ReadActivity.class);
@@ -51,5 +73,31 @@ public class MainActivity extends AppCompatActivity {
     private void openActivity(Class<?> activityClass) {
         Intent intent = new Intent(MainActivity.this, activityClass);
         startActivity(intent);
+    }
+
+    private void openActivityPassingType(Class<?> activityClass, String typeActivity) {
+        Intent intent = new Intent(MainActivity.this, activityClass);
+        Bundle bundle = new Bundle();
+        bundle.putString("typeActivity", typeActivity);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private NavigationView.OnNavigationItemSelectedListener onItemSelect() {
+        return item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_read:
+                    openActivityPassingType(ReadAndGeneratedQRCodes.class, "read");
+                    break;
+                case R.id.nav_generate:
+                    openActivityPassingType(ReadAndGeneratedQRCodes.class, "generated");
+                    break;
+                case R.id.nav_configuration:
+                    openActivity(ConfigurationActivity.class);
+                    break;
+            }
+            return false;
+        };
     }
 }
---
+++ app/src/main/java/com/emanuelgalvao/qrcodeapp/ReadAndGeneratedQRCodes.java
@@ -0,0 +1,14 @@
+package com.emanuelgalvao.qrcodeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadAndGeneratedQRCodes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_and_generated_q_r_codes);
+    }
+}
